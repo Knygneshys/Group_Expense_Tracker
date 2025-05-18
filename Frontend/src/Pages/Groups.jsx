4619835301fd973485cf745d05d0686d800e647a
Knygneshys <lukasmieliauskas@gmail.com>
2025-05-18 14:23:07 +0300

    Renamed App.jsx to Groups.jsx and moved it to the Pages folder

--- Frontend/src/Pages/Groups.jsx
+++ Frontend/src/Pages/Groups.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import reactLogo from '../assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
-import GroupList from './Lists/GroupList.jsx'
+import './Groups.css'
+import GroupList from '../Lists/GroupList.jsx'
 // (parameters) => some code
 
 
 
-function App() {
+function Groups() {
   document.title = `Group Finance Tracker`;
   return (
     <>
@@ -18,4 +18,4 @@ function App() {
 }
 
 
-export default App
+export default Groups
