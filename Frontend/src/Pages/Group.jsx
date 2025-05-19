a99346be85555a28af71a273de30237ffa277af5
Knygneshys <lukasmieliauskas@gmail.com>
2025-05-19 12:01:58 +0300

    Group page correctly fetches and gets group by id

--- Frontend/src/Pages/Group.jsx
+++ Frontend/src/Pages/Group.jsx
@@ -1,12 +1,15 @@
 import { useParams } from "react-router-dom";
-
+const apiUrl = import.meta.env.VITE_API_URL;
 
 function Group()
 {
+
+
     const renderMessage = () => {
         const {id} = useParams();
-        const string = JSON.stringify(id);
-        if(string != "{}") {
+        const groupId = parseInt(id, 10);
+        if(Number.isInteger(groupId)) {
+            fetchGroup(groupId);
             return <p>{id}</p>;
         } 
         else {
@@ -18,6 +21,23 @@ function Group()
             {renderMessage()}
         </>
     )
+}
+
+async function fetchGroup(id)
+{
+    try{
+        console.log(id)
+        const response = await fetch(`${apiUrl}/api/Groups/${id}`);
+        if(!response.ok) {
+            throw new Error(`Failed to fetch group by id: ${id}`);
+        }
+
+        const data = await response.json();
+        console.log(data);
+    }
+    catch(error){
+        console.log(error);
+    }
 }
 
 export default Group;
